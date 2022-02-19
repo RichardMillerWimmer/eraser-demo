b17353a81dbfef0a120e3c447fbf604c0926d2cc
Richard Miller <richardmmill@gmail.com>
2022-02-18 21:53:34 -0500

    button functionality

--- client/js/index.js
+++ client/js/index.js
@@ -1,1 +1,9 @@
 $('#foreground').eraser();
+
+document.getElementById('resetBtn').onclick = function () {
+    $('#foreground').eraser('reset');
+};
+
+document.getElementById('clearBtn').onclick = function () {
+    $('#foreground').eraser('clear');
+};
